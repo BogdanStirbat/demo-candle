13ec9d8289b5c0701e414e2f537c0378691da5c3
Bogdan Stirbat <bogdan.stirbat@gmail.com>
2021-05-27 11:32:29 +0300

    Navbar logo has a link to Home page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { Flex, Text, Icon, Image } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 import { MdMenu, MdShoppingBasket } from 'react-icons/md'
 
 import { ShopContext } from '../context/shopContext'
@@ -11,7 +12,9 @@ const NavBar = () => {
   return (
     <Flex backgroundColor="#B5B7B4" flexDir="row" justifyContent="space-between" p="2rem">
       <Icon fill="white" cursor="pointer" as={MdMenu} w={30} h={30} ></Icon>
-      <Image src="https://cdn.shopify.com/s/files/1/0571/7793/1948/files/demo-candle-logo.svg?v=1622060749" w={150} h={150} />
+      <Link to="/">
+        <Image src="https://cdn.shopify.com/s/files/1/0571/7793/1948/files/demo-candle-logo.svg?v=1622060749" w={150} h={150} />
+      </Link>
       <Icon fill="white" cursor="pointer" as={MdShoppingBasket} w={30} h={30} onClick={ () => openCart() }  ></Icon>
     </Flex>
   )
